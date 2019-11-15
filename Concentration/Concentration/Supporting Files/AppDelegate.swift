cbc98dd7e25537151be27c6a383e23ea33cafa70
adamscout162 <adam.israfil@gmail.com>
2019-11-14 18:26:11 -0800

    Created project

---
+++ Concentration/Concentration/Supporting Files/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Concentration
+//
+//  Created by Adam Israfil on 11/14/19.
+//  Copyright © 2019 Adam Israfil. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Adam Israfil on 11/14/19.
+//  Copyright © 2019 Adam Israfil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
