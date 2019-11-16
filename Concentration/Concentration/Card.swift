b6689c0474611fb7a9ff8873c2dcb70f4fee8005
Unknown <adam.israfil@gmail.com>
2019-11-15 17:24:53 -0800

    End of Lecture 1, plus added all UI elements

---
+++ Concentration/Concentration/Card.swift
@@ -0,0 +1,27 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Adam Israfil on 11/15/19.
+//  Copyright © 2019 Adam Israfil. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
