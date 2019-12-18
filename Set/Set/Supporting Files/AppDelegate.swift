c7d40e55a96256ab5361529dea34cc5166f60a9b
Unknown <adam.israfil@gmail.com>
2019-12-17 23:45:25 -0800

    UI of Set

---
+++ Set/Set/Supporting Files/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Set
+//
+//  Created by Adam Israfil on 12/8/19.
+//  Copyright © 2019 Adam Israfil. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    var window: UIWindow?
+}
+
---
+++ Set/Set/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Adam Israfil on 12/8/19.
+//  Copyright © 2019 Adam Israfil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let screenSize: CGRect = UIScreen.main.bounds
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var drawCardsButton: UIButton!
+    @IBOutlet weak var newGameButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func didPressDrawCardsButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func didPressNewGameButton(_ sender: UIButton) {
+        
+    }
+
+}
+
