3e046e9e28e8801279676ed92785880be4993f8d
Unknown <adam.israfil@gmail.com>
2020-02-01 21:05:04 -0500

    End of Lecture 9

---
+++ Cassini/Cassini/ImageViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Adam Israfil on 1/31/20.
+//  Copyright © 2020 Adam Israfil. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController, UIScrollViewDelegate {
+    
+    var imageURL: URL? {
+        didSet {
+            imageView.image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            
+            imageView.sizeToFit()
+            scrollView.contentSize = imageView.frame.size
+        }
+    }
+    
+    var imageView = UIImageView()
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.minimumZoomScale = 1/25
+            scrollView.maximumZoomScale = 1.0
+            scrollView.delegate = self
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if imageURL == nil {
+            imageURL = DemoURLs.stanford
+        }
+    }
+}
